b9c7b94ef1435280f5cd4d571c10767971687cfd
DElAmin <delamin13@gmail.com>
2021-02-03 12:59:20 +0100

    React ProtoTypes and isRequired

--- react_task_tracker/src/components/Header.js
+++ react_task_tracker/src/components/Header.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 //const Header = (props) => {
 const Header = ({title}) => {
         return (
@@ -9,6 +11,10 @@ const Header = ({title}) => {
 
 Header.defaultProps = {
     title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
 }
 
 export default Header
