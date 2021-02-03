c29caec4c27785f67a9c6c8d1081f96ce8915709
DElAmin <delamin13@gmail.com>
2021-02-03 14:16:41 +0100

    React Styling and adding Button component

--- react_task_tracker/src/components/Header.js
+++ react_task_tracker/src/components/Header.js
@@ -1,10 +1,14 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 //const Header = (props) => {
 const Header = ({title}) => {
         return (
-        <header>
+        <header className='header'>
+            {/* <h1 style={{color: 'red'}}>{title}</h1> */}
+            {/* <h1 style={headingStyle}>{title}</h1> */}
             <h1>{title}</h1>
+            <Button color='green' text='Add' />
         </header>
     )
 }
@@ -16,5 +20,11 @@ Header.defaultProps = {
 Header.propTypes = {
     title: PropTypes.string.isRequired,
 }
+
+//CSS in JS
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black',
+// }
 
 export default Header
